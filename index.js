2d0a4b364a2b9280697555b6f5d570f36208d537
Lakshay Sethi <8218018-lakshaynz@users.noreply.gitlab.com>
2022-11-09 21:42:03 +1300

    connected to mongo

--- index.js
+++ index.js
@@ -2,8 +2,47 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+mongoose.connect("mongodb://localhost:27017/MoviesDB")
+    .then(() => console.log("data connect successfully"))
+    .catch((err) => console.log("there was an error", err))
 
-mongoose.connect("mongodb+srv://test:test@cluster0.53zblji.mongodb.net/test")
-.then(()=>console.log("data connect successfully"))
-.catch((err)=>console.log("htere was error",err))
+const app = express()
+
+app.use(express.json()) // when request comes it will be parsed by the express into json and when we send a response this will also convert it into string 
+app.use(cors()) // will handle the cors for our request and responses 
+
+// schema for movies
+const movieSchema = new mongoose.Schema({
+
+    moviename: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    releasedate: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+// model  for schema
+const movieModel = new mongoose.model('Movies', movieSchema)
+
+
+app.get('/',(req,res)=>{
+    movieModel.find()
+    .then((data)=>{
+
+        // console.log(data)
+        res.send(data)
+    })
+    .catch((err) => console.log("there was an error", err))
+
+})
+
+
+app.listen(8000)
 
